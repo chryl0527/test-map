98bccfb62cce59d8a7a6613d7de700bc3e3fab10
chr0527 <1015670600@qq.com>
2019-05-21 15:09:46 +0800

    init

---
+++ test-cyl/src/main/java/com/sdsoon/TestCylApplication.java
@@ -0,0 +1,13 @@
+package com.sdsoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCylApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCylApplication.class, args);
+	}
+
+}

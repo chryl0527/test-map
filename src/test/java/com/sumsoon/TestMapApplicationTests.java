fee66e4e499c754380e75ce41111c4e639997911
Chr0527 <1015670600@qq.com>
2019-07-10 16:43:35 +0800

    init

--- src/test/java/com/sumsoon/TestMapApplicationTests.java
+++ src/test/java/com/sumsoon/TestMapApplicationTests.java
@@ -1,16 +1,25 @@
 package com.sumsoon;
 
+import com.sumsoon.zzz.mapper.ClaStuMapper;
+import com.sumsoon.zzz.po.Country;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestMapApplicationTests {
+    @Autowired
+    private ClaStuMapper claStuMapper;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        List<Country> countries = claStuMapper.queryCPCC();
+        System.out.println(countries);
+    }
 
 }
